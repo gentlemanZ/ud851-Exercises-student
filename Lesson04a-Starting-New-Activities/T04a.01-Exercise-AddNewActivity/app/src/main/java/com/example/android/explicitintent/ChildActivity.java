b38b6f6d740d1163ff8b376e366ee8712c3f942a
Tianyang Zhong <tianyangzhong2013@gmail.com>
2017-08-21 16:21:28 -0400

    finish exercise lesson04a, Explicit Intent

---
+++ Lesson04a-Starting-New-Activities/T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,17 @@
+package com.example.android.explicitintent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class ChildActivity extends AppCompatActivity {
+    private TextView mDisplayMessage;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+        mDisplayMessage = (TextView) findViewById(R.id.tv_display);
+    }
+}
